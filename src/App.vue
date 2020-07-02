16bd5c0bfdb2bdab54fb1919641ff15d37f884ff
aso1901104 <1901104@s.asojuku.ac.jp>
2020-07-02 05:49:16 +0000

    unfinish

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mylist v-bind:message="message" v-on:result-event="appAction"/>
+      <hr>
+      <div class="result">
+      <table v-html="log" align="center"></table>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mylist from './components/Mylist.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Mylist
+  },
+  data:function(){
+    return {
+      message: 'メモを入力してください',
+      result:[],
+    };
+  },
+  computed:{
+    log:function(){
+      var table='<tr><th class="head">my list</th></tr>';
+      for(var i in this.result){
+        table += '<tr><td>' + this.result[i] + '</td></tr>';
+      }
+      return table;
+    }
+  },
+  created:function(){
+    var items = localStorage.getItem('log');
+    var logs = JSON.parse(items);
+    if(logs != null){ this.result = logs; }
+  },
+  methods:{
+    appAction:function(exp, res){
+      this.result.unshift([exp, res]);
+      if(this.result.length>5){
+        this.result.pop();
+      }
+      var log = JSON.stringify(this.result);
+      localStorage.setItem('log', log);
+    }
   }
 }
 </script>
@@ -24,5 +57,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+table{
+  border:1px solid gray;
+}
+tr th.head{
+  background-color:black;
+  color:white;
 }
 </style>
---
+++ src/components/Mylist.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h3>{{ message }}</h3>
+        <div>
+        <input type="text" v-model="input">
+        <button v-on:click="doAction">クリック</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Mylist',
+    props:{
+        message: String,
+    },
+    data:function(){
+    return {
+      input:'input here.',
+    };
+  },
+  methods:{
+    doAction: function(){
+      this.$emit('result-event', this.input);
+    }
+  }
+}
+</script>
+
+<style>
+div{
+  margin:0px;
+  padding:0px;
+  text-align:center;
+}
+p{
+  margin:0px;
+  color:#666;
+  font-size:16pt;
+}
+</style>
